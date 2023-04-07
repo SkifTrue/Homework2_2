6f05554d71e69b9a52de14e088a3da571ee3f473
Vasili <vasili.trutnev@gmail.com>
2023-04-07 20:31:10 +0500

    Сorrection

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int money1 = 100; // Изначальное колличество денег на счету
+        int moneyFrst = 100; // Изначальное колличество денег на счету
         int refill = 1001; // Пополнение
 
         int bonus = refill / 100; // Бонусы
-        int bonus_calc = refill > 1000 ? (bonus) : (0); // Расчет бонусов
-        int full = money1 + refill + bonus_calc; // Общее колличество денег на счету
+        int bonusCalc = refill > 1000 ? (bonus) : (0); // Расчет бонусов
+        int full = moneyFrst + refill + bonusCalc; // Общее колличество денег на счету
 
         System.out.println("Общая сумма на счету " + full);
 
         System.out.println("Пополнение на сумму " + refill);
 
-        System.out.println("Колличество бонусных рублей " + bonus_calc);
+        System.out.println("Колличество бонусных рублей " + bonusCalc);
     }
 }
